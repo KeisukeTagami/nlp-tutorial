06a8372529954fe813a0e348fc2b0a7306687e19
Keisuke Tagami <suruji42@gmail.com>
2019-05-16 01:06:44 +0900

    add 4-1 files

---
+++ 4-1.Seq2Seq/NLPDataset.cpp
@@ -0,0 +1,117 @@
+
+#include "NLPDataset.h"
+
+#include <torch/data/example.h>
+#include <torch/types.h>
+#include <c10/util/Exception.h>
+
+#include <cstddef>
+#include <fstream>
+#include <string>
+#include <vector>
+
+namespace torch {
+namespace data {
+namespace datasets {
+namespace {
+constexpr uint32_t kInit = 3;
+constexpr uint32_t kLast = 1;
+
+} // namespace
+
+  NLP::NLP(std::vector<std::pair<std::string, std::string>> data_seq) {
+
+    std::vector<int64_t> concat_input_words;
+    std::vector<int64_t> concat_output_words;
+    std::vector<int64_t> concat_target_words;
+
+  int index=0;
+  for( auto c : std::string("SEPabcdefghijklmnopqrstuvwxyz") ) {
+    std::string str = std::string(1, c);
+    words.insert(str);
+    word_index[str] = index;
+    index_word[index] = str;
+    index++;
+  }
+
+  uint64_t input_max = 0;
+  uint64_t output_max = 0;
+
+  for( auto data : data_seq ) {
+    auto input = data.first;
+    auto output = data.second;
+    input_max = std::max(input_max, input.length());
+    output_max = std::max(output_max, output.length());
+  }
+
+  for( auto data : data_seq ) {
+
+    {
+      int i = 0;
+      for( auto c : data.first ) {
+        std::string str = std::string(1, c);
+        concat_input_words.push_back(word_index[str]);
+        i++;
+      }
+      for( ; i < input_max ; i++ ) {
+        concat_input_words.push_back(word_index["P"]);
+      }
+    }
+
+    {
+      int i = 0;
+      concat_output_words.push_back(word_index["S"]);
+      for( auto c : data.second ) {
+        std::string str = std::string(1, c);
+        concat_output_words.push_back(word_index[str]);
+        concat_target_words.push_back(word_index[str]);
+        i++;
+      }
+      for( ; i < output_max ; i++ ) {
+        concat_output_words.push_back(word_index["P"]);
+        concat_target_words.push_back(word_index["P"]);
+      }
+      concat_target_words.push_back(word_index["E"]);
+    }
+  }
+
+  const uint64_t count = data_seq.size();
+  torch::Tensor input = torch::empty({count, input_max}, torch::kInt64);
+  torch::Tensor output = torch::empty({count, output_max}, torch::kInt64);
+  torch::Tensor target = torch::empty({count, output_max}, torch::kInt64);
+  std::memcpy(input.data_ptr(), concat_input_words.data(), input.numel() * sizeof(int64_t));
+  std::memcpy(output.data_ptr(), concat_output_words.data(), output.numel() * sizeof(int64_t));
+  std::memcpy(target.data_ptr(), concat_output_words.data(), target.numel() * sizeof(int64_t));
+  input_   = input;
+  targets_ = torch::cat({output, target}, 0);
+}
+
+int64_t NLP::getClassNumber() {
+  return words.size();
+}
+
+Example<> NLP::get(size_t index) {
+  auto one_hot = torch::one_hot(input_[index], getClassNumber()).to(torch::kFloat);
+  return {at::cat({one_hot, one_hot}), targets_[index]};
+}
+
+optional<size_t> NLP::size() const {
+  return input_.size(0);
+}
+
+const Tensor& NLP::input() const {
+  return input_;
+}
+
+const Tensor& NLP::targets() const {
+  return targets_;
+}
+
+const std::string& NLP::index_to_string(int64_t index) {
+  return index_word[index];
+}
+
+
+} // namespace datasets
+} // namespace data
+} // namespace torch
---
+++ 4-1.Seq2Seq/Seq2Seq_Torch.cpp
@@ -0,0 +1,159 @@
+/********************************************
+ * $ mkdir build
+ * $ cd build
+ * $ cmake -DCMAKE_PREFIX_PATH=/path/to/libtorch ..
+ * $ make
+ **********************************************/
+
+#include <torch/torch.h>
+
+#include <cstddef>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "NLPDataset.h"
+
+// The number of epochs to train.
+const int64_t kNumberOfEpochs = 5000;
+
+// After how many batches to log a new update with the loss value.
+const int64_t kLogInterval = 1000;
+
+const int64_t n_hidden = 5;
+
+std::shared_ptr<torch::nn::RNNImpl> make_rnn(int64_t input_size, int64_t hidden_size) {
+  auto options = torch::nn::RNNOptions(input_size, hidden_size);
+  options.dropout(0.5);
+  return std::make_shared<torch::nn::RNNImpl>(options);
+}
+
+std::shared_ptr<torch::nn::LinearImpl> make_linear(int64_t input_size, int64_t hidden_size) {
+  auto options = torch::nn::LinearOptions(input_size, hidden_size);
+  return std::make_shared<torch::nn::LinearImpl>(options);
+}
+
+
+struct Net : torch::nn::Module {
+  Net(int64_t n_class)
+    : enc_cell(make_rnn(n_class, n_hidden)),
+      dec_cell(make_rnn(n_class, n_hidden)),
+      fc(make_linear(n_hidden, n_class))
+  {
+    register_module("enc_cell", enc_cell);
+    register_module("dec_cell", dec_cell);
+    register_module("fc", fc);
+  }
+
+  torch::Tensor forward(torch::Tensor enc_input, torch::Tensor enc_hidden, torch::Tensor dec_input) {
+    enc_input = enc_input.transpose(0, 1); // enc_input : [n_step, batch_size, n_class]
+    dec_input = dec_input.transpose(0, 1); // enc_input : [n_step, batch_size, n_class]
+    auto enc_output = enc_cell->forward(enc_input, enc_hidden);
+    auto dec_output = dec_cell->forward(dec_input, enc_output.state);
+    auto output = fc->forward(dec_output.output);
+    output = torch::log_softmax(output, /*dim=*/1);
+    return output;
+  }
+
+  std::shared_ptr<torch::nn::RNNImpl> enc_cell;
+  std::shared_ptr<torch::nn::RNNImpl> dec_cell;
+  std::shared_ptr<torch::nn::LinearImpl> fc;
+};
+
+auto main() -> int {
+  torch::manual_seed(1);
+
+  torch::DeviceType device_type;
+  if (torch::cuda::is_available()) {
+    std::cout << "CUDA available! Training on GPU." << std::endl;
+    device_type = torch::kCUDA;
+  } else {
+    std::cout << "Training on CPU." << std::endl;
+    device_type = torch::kCPU;
+  }
+  torch::Device device(device_type);
+
+  std::vector<std::pair<std::string, std::string>>
+    seq_data {{"man", "women"},
+              {"black", "white"},
+              {"king", "queen"},
+              {"girl", "boy"},
+              {"up", "down"},
+              {"high", "low"}};
+
+  const int64_t batch_size = static_cast<int64_t>(seq_data.size());
+
+  auto dataset = torch::data::datasets::NLP(seq_data);
+  auto train_dataset = dataset.map(torch::data::transforms::Stack<>());
+  auto data_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(std::move(train_dataset), batch_size);
+
+ int64_t nClass = dataset.getClassNumber();
+  Net model(nClass);
+
+  model.to(device);
+  torch::optim::Adam optimizer(model.parameters(), torch::optim::AdamOptions(0.001));
+  model.train();
+
+  for (size_t epoch = 1; epoch <= kNumberOfEpochs; ++epoch) {
+
+    float loss_value;
+    torch::Tensor loss;
+
+    for (auto& batch : *data_loader) {
+      auto data = batch.data.to(device);
+      auto targets = batch.target.to(device);
+      auto hidden = torch::Tensor(torch::zeros({1, batch_size, n_hidden}));
+
+      optimizer.zero_grad();
+      auto output = model.forward(data, hidden, targets[0]);
+      output = output.transpose(0, 1); // [batch_size, max_len+1(=6), num_directions(=1) * n_hidden]
+
+      for( int i = 0 ; i < batch_size; i++ )
+        loss += torch::nll_loss(output[i], targets[i]);
+      AT_ASSERT(!std::isnan(loss.template item<float>()));
+      loss.backward();
+      optimizer.step();
+
+      loss_value = loss.template item<float>();
+    }
+
+    if (epoch % kLogInterval == 0) {
+      std::cout << std::endl;
+      std::cout << "\rTrain Epoch: " << epoch
+                << "[" << std::setfill(' ') << std::setw(5) << epoch
+                << "/" << std::setfill(' ') << std::setw(5) << kNumberOfEpochs
+                << "]"
+                << " Loss: " <<  loss_value;
+    }
+  }
+
+  model.eval();
+
+  for (auto& batch : *data_loader) {
+    auto input = batch.data.to(device);
+    auto targets = batch.target.to(device);
+    auto hidden = torch::Tensor(torch::zeros({1, batch_size, n_hidden}));
+    auto predict = model.forward(input, hidden, targets[0]);
+    input = input.argmax(2).cpu();
+    targets = targets.cpu();
+    predict = predict.argmax(1).cpu();
+
+    auto input_accessor = input.accessor<int64_t,2>();
+    auto targets_accessor = targets.accessor<int64_t,1>();
+    auto predict_accessor = predict.accessor<int64_t,1>();
+
+    std::cout << std::endl;
+    for(int i = 0; i < input_accessor.size(0); i++) {
+      for(int j = 0; j < input_accessor.size(1); j++) {
+        std::cout << dataset.index_to_string(input_accessor[i][j]);
+      }
+      std::cout << " ";
+      std::cout << dataset.index_to_string(predict_accessor[i]);
+      std::cout << " [" << dataset.index_to_string(targets_accessor[i]) << "]";
+      std::cout << std::endl;
+    }
+  }
+}
+
+
